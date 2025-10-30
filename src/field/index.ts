3ce0dcb5047f0f6ca1673b11c918590c65758e72
Joshua Bernstein <joshuatbernstein@gmail.com>
2025-10-29 22:37:55 -0400

    moved packages around

--- src/field/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./field.js";
-import PrimaryKey from "./primary-key.js";
-export { PrimaryKey };
-
-export * from "./relation.js";
-export * from "./field-types.js";
--- src/model/index.ts
+++
@@ -1,3 +0,0 @@
-import Model from "./model.js";
-export * from "./model-types.js";
-export { Model };
